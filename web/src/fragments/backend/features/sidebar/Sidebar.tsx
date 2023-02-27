763615d701316c9896d809f51c9b2bdf9dc15a1f
ADMBP <devopadmbp@gmail.com>
2023-02-27 17:07:22 -0300

    Update css e js

--- web/src/fragments/backend/features/sidebar/Sidebar.tsx
+++ web/src/fragments/backend/features/sidebar/Sidebar.tsx
@@ -43,19 +43,19 @@ export const Sidebar= () => {
                             </a>
                             <ul className="nav nav-treeview">
                             <li className="nav-item">
-                                <a href="./index.html" className="nav-link">
+                                <a href="#" className="nav-link">
                                 <i className="far fa-circle nav-icon"></i>
                                 <p>Dashboard v1</p>
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a href="./index2.html" className="nav-link">
+                                <a href="#" className="nav-link">
                                 <i className="far fa-circle nav-icon"></i>
                                 <p>Dashboard v2</p>
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a href="./index3.html" className="nav-link">
+                                <a href="#" className="nav-link">
                                 <i className="far fa-circle nav-icon"></i>
                                 <p>Dashboard v3</p>
                                 </a>
--- web/src/pages/backend/dashboard/Dashboard.tsx
+++ web/src/pages/backend/dashboard/Dashboard.tsx
@@ -3,15 +3,13 @@ import { Footer, Navbar, Panel, Sidebar } from "../../../fragments"
 
 export const Dashboard = () => {
     return (
-        <div className="hold-transition sidebar-mini layout-fixed dark-mode" data-panel-auto-height-mode="height">
-            <div className="wrapper">
-                <Navbar />
-                <Sidebar />
-                <Panel />
-                <Footer />
-                <aside className="control-sidebar control-sidebar-dark">
-                </aside>
-            </div>
+        <div className="wrapper">
+            <Navbar />
+            <Sidebar />
+            <Panel />
+            <Footer />
+            <aside className="control-sidebar control-sidebar-dark">
+            </aside>
         </div>
     )
 }
